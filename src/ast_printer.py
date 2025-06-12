17bc2c0aedede19506675b43398390fa4c8998e8
benjamin-feldman <benjamin.feldman666@gmail.com>
2025-06-12 13:50:49 +0200

    add AstPrinter visitor

---
+++ src/ast_printer.py
@@ -0,0 +1,51 @@
+from typing import override
+from expr import Visitor, Expr
+from src.expr import Binary, Grouping, Literal, Unary
+
+
+class AstPrinter(Visitor[str]):
+    def print(self, expr: Expr):
+        return expr.accept(self)
+
+    @override
+    def visit_binary_expr(self, expr: Binary) -> str:
+        return self._parenthesize(expr.operator.lexeme, expr.left, expr.right)
+
+    @override
+    def visit_grouping_expr(self, expr: Grouping) -> str:
+        return self._parenthesize("group", expr.expression)
+
+    @override
+    def visit_literal_expr(self, expr: Literal) -> str:
+        if not expr.value:
+            return "nil"
+        return str(expr.value)
+
+    @override
+    def visit_unary_expr(self, expr: Unary) -> str:
+        return self._parenthesize(expr.operator.lexeme, expr.right)
+
+    def _parenthesize(self, name: str, *exprs: Expr) -> str:
+        res = "("
+        res += name
+        for expr in exprs:
+            res += " "
+            res += expr.accept(self)
+
+        res += ")"
+
+        return res
+
+
+if __name__ == "__main__":
+    from src.token import Token, TokenType
+    from src.expr import Binary, Unary, Literal, Grouping
+
+    expression = Binary(
+        left=Unary(operator=Token(TokenType.MINUS, "-", None, 1), right=Literal(123)),
+        operator=Token(TokenType.STAR, "*", None, 1),
+        right=Grouping(expression=Literal(45.67)),
+    )
+
+    printer = AstPrinter()
+    print(printer.print(expression))
